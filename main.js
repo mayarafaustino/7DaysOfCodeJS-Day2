6dcc1c3195d99c47d0990b3007293f2537d5fa71
Mayara <mayarafvieira@gmail.com>
2022-03-30 18:08:23 -0300

    all

---
+++ main.js
@@ -0,0 +1,31 @@
+const divMsg = document.getElementById("mensagem-ola");
+const divResposta = document.getElementById("resposta");
+
+function mandarMsg() {
+    const nome = document.getElementById("input-nome").value;
+    const idade = document.getElementById("input-idade").value;
+    const linguagem = document.getElementById("input-linguagem").value;
+
+    if (nome != "" && idade != "" && linguagem != "") {
+        const msg = `<p class='msg-ola'>Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!</p>
+       <p>Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.</p>
+       <input type='number' id='input-numero'><button type='button'onclick='mandarResposta()'>enviar</button>`
+
+        divMsg.innerHTML = msg;
+        
+
+    }
+}
+function mandarResposta() {
+    const numero = document.getElementById("input-numero").value;
+    if (numero == 1) {
+        const resposta = "Muito bom! Continue estudando e você terá muito sucesso.";
+        divResposta.innerHTML = resposta;
+    } else if (numero == 2) {
+        const resposta = "Ahh que pena... Já tentou aprender outras linguagens?";
+        divResposta.innerHTML = resposta;
+    } else {
+        const resposta = "Resposta inválida";
+        divResposta.innerHTML = resposta;
+    }
+}
